478fc72e203c38c4c51f4ef4533569bf2a32000e
thuhuong050903 <116275747+thuhuong050903@users.noreply.github.com>
2022-11-22 11:27:41 +0700

    change

--- JavaScript/slider.js
+++ JavaScript/slider.js
@@ -1,12 +1,15 @@
 var swiper = new Swiper(".mySwiper", {
-    cssMode: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
-  });
+  slidesPerView: 5,
+  spaceBetween: 30,
+  slidesPerGroup: 5,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
